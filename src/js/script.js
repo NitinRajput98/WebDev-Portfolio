f3ab7ced4fe5c93c8b9a16d58783cd5e4d43f6ca
Rajput <nitin98811234@gmail.com>
2024-05-04 23:51:05 +0530

    Projects section completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,14 @@
 var swiper = new Swiper(".mySwiper", {
+  rewind: true,
   cssMode: true,
+  // effect: "fade",
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   mousewheel: true,
   keyboard: true,
