1b910bb00d53773689ab007212fd05e60ec3fe38
Rajput <nitin98811234@gmail.com>
2024-05-05 05:01:52 +0530

    Dark mode completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Swiper code
 var swiper = new Swiper(".mySwiper", {
   rewind: true,
   cssMode: true,
@@ -12,4 +13,46 @@ var swiper = new Swiper(".mySwiper", {
   },
   mousewheel: true,
   keyboard: true,
+});
+////////////////////////////////////////////////
+
+const navLinks = document.querySelector(".navlinks");
+const navItems = [...document.querySelectorAll(".nav-item")];
+const scrollToTopBtn = document.querySelector(".back-to-top");
+const switchTheme = document.querySelector(".switch");
+
+navLinks.addEventListener("mouseover", function (e) {
+  if (e.target === navLinks) return;
+  navItems.forEach((el) => {
+    if (el !== e.target) {
+      el.classList.add("anchor-blur");
+    }
+  });
+});
+
+navLinks.addEventListener("mouseout", function (e) {
+  if (e.target === navLinks) return;
+  navItems.forEach((el) => {
+    if (el !== e.target) {
+      el.classList.remove("anchor-blur");
+    }
+  });
+});
+
+navLinks.addEventListener("click", function (e) {
+  // e.preventDefault();
+  if (e.target.classList.contains("nav-item")) {
+    document
+      .querySelector(e.target.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+scrollToTopBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+switchTheme.addEventListener("click", function (e) {
+  console.log("Click");
 });
